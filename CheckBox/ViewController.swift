94568e0a7c2fe7d0c07c2a47b632cbc48322a308
mi81ma <mi81ma@gmail.com>
2019-03-04 18:49:14 +0800

    initial commit

--- CheckBox/ViewController.swift
+++ CheckBox/ViewController.swift
@@ -9,10 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var checkbox = UIButton.init(type: .custom)
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        checkbox = UIButton(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
+
+        checkbox.imageView?.sizeToFit()
+
+        checkbox.setImage(UIImage.init(named: "iconCheckboxOutlined"), for: .normal)
+        checkbox.setImage(UIImage.init(named: "iconCheckboxFilled"), for: .selected)
+        checkbox.addTarget(self, action: #selector(self.toggleCheckboxSelection), for: .touchUpInside)
+
+        view.addSubview(checkbox)
+    }
+
+    @objc func toggleCheckboxSelection() {
+    checkbox.isSelected = !checkbox.isSelected
     }
 
 
